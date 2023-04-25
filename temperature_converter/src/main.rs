5c2cfec48fd7fa68e49de318aa6929b82f9b8ed6
BenedictusAryo <benedictusaryo@outlook.com>
2023-04-25 17:13:21 +0700

    temperature converter celcius fahrenheit and vice versa

--- temperature_converter/src/main.rs
+++ temperature_converter/src/main.rs
@@ -1,10 +1,48 @@
 use std::io;
 
+static TITLE: &str = "
+Temperature Converter
+---------------------
+Choose option:
+1) Celcius to Fahrenheit
+2) Fahrenheit to Celcius
+";
+
+enum OptionChoice {
+    One,
+    Two
+}
+
 fn main() {
-    println!("Celcius to Fahrenheit Converter\n");
+    println!("{TITLE}");
+
+    // Choose Option
+    let option: OptionChoice = loop {
+        println!("Input option:");
+        let mut choosen = String::new();
+        io::stdin()
+            .read_line(&mut choosen)
+            .expect("Failed to read input");
+
+        match choosen.trim().parse::<u32>() {
+            Ok(1) => {
+                println!("You choose Option 1\n");
+                break OptionChoice::One;
+            },
+            Ok(2) => {
+                println!("You choose Option 2\n");
+                break OptionChoice::Two;
+            },
+            Ok(_) => println!("Please insert number of given option!\n"),
+            Err(_) => {
+                println!("Please insert a valid number!\n");
+            }
+        };
+    };
     
-    let celcius = loop {
-        println!("Input Celcius degree:");
+    // Input degree
+    let input_degree = loop {
+        println!("Input degree:");
         let mut input = String::new();
         io::stdin()
             .read_line(&mut input)
@@ -13,15 +51,31 @@ fn main() {
         match input.trim().parse::<f64>() {
             Ok(num) => break num,
             Err(_) => {
-                println!("Please insert number input!");
+                println!("Please insert number input!\n");
                 continue;
             }
         };
     };
-    let fahrenheit = celcius_to_fahrenheit(celcius);
-    println!("{celcius} degree Celcius equals to {fahrenheit} degree Fahrenheit");
+
+    // Convert the input using the correct converter function
+    // based on the user's choice
+    let result = match option {
+        OptionChoice::One => celcius_to_fahrenheit(input_degree),
+        OptionChoice::Two => fahrenheit_to_celcius(input_degree),
+    };
+
+    // Print the result
+    match option {
+        OptionChoice::One => println!("\nResult:\n{input_degree} degree Celcius equals to {result}, degree Fahrenheit"),
+        OptionChoice::Two => println!("\nResult:\n{input_degree} degree Fahrenheit equals to {result}, degree Celcius"),
+    }
+
 }
 
 fn celcius_to_fahrenheit(celcius: f64) -> f64 {
     celcius * (9.0/5.0) + 32.0
+}
+
+fn fahrenheit_to_celcius(fahrenheit: f64) -> f64 {
+    (fahrenheit - 32.0) * (5.0/9.0)
 }
