68a60dd8e9c4dfd46814744b5b9568476953275f
BenedictusAryo <benedictusaryo@outlook.com>
2023-04-26 20:27:47 +0700

    run fibonacci using loop u64

--- fibonacci_number/src/main.rs
+++ fibonacci_number/src/main.rs
@@ -17,23 +17,38 @@ fn main() {
             .read_line(&mut input_number)
             .expect("Failed to read input");
 
-        match input_number.trim().parse::<u32>() {
+        match input_number.trim().parse::<u64>() {
             Ok(0) => {
                 println!("Please insert number greater than 0!\n");
                 continue;
             },
             Ok(num) => break num,
             Err(_) => {
-                println!("Please insert number input!\n");
+                println!("Please insert positive number input!\n");
                 continue;
             }
         }
     };
-    println!("Requested number of sequence: {input_number}")
+
+    // Run Fibonacci Generator
+    println!("\nRequested number of sequence: {input_number}");
+    let fibonacci_sequence = generate_fibonacci_sequence(input_number);
+    println!("Generated Fibonacci Sequence: {fibonacci_sequence:?}");
+
 }
 
-// fn generate_fibonacci_sequence(number: u32) -> Vec<u32> {
-//     let mut sequence: Vec<u32> = Vec::new();
+fn generate_fibonacci_sequence(number: u64) -> Vec<u64> {
+    let mut sequence: Vec<u64> = Vec::new();
 
-    
-// }
+    let mut num1 = 0;
+    let mut num2 = 1;
+    let mut num3;
+
+    for _ in 0..number {
+        sequence.push(num1);
+        num3 = num1 + num2;
+        num1 = num2;
+        num2 = num3;
+    }
+    sequence
+}
